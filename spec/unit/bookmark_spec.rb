4b79bbfa164e18151257096a4a90a552a2994046
Adam Jonas <adamjonas25@gmail.com>
2021-02-22 22:36:25 +0000

    add test db stub to unit spec

--- spec/unit/bookmark_spec.rb
+++ spec/unit/bookmark_spec.rb
@@ -1,4 +1,12 @@
 describe Bookmark do
+  before do
+    allow(Bookmark).to receive(:all) do
+      connection = PG.connect(dbname: 'bookmark_manager_test')
+      result = connection.exec('SELECT * FROM bookmarks;')
+      result.map { |row| row['url'] }
+    end
+  end
+
   describe '.all' do
     it 'returns all the bookmarks' do
       expect(described_class.all).to include 'http://www.makersacademy.com'
