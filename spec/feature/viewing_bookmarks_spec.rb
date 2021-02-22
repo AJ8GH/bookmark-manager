226a61aac183fc3473fed21f60592bee76816178
Adam Jonas <adamjonas25@gmail.com>
2021-02-22 22:35:41 +0000

    add test db stub to feature specs

--- spec/feature/viewing_bookmarks_spec.rb
+++ spec/feature/viewing_bookmarks_spec.rb
@@ -1,4 +1,12 @@
 feature 'viewing bookmarks' do
+  before do
+    allow(Bookmark).to receive(:all) do
+      connection = PG.connect(dbname: 'bookmark_manager_test')
+      result = connection.exec('SELECT * FROM bookmarks;')
+      result.map { |row| row['url'] }
+    end
+  end
+
   scenario 'visiting index page' do
     visit '/'
     expect(page).to have_content 'Bookmark Manager'
